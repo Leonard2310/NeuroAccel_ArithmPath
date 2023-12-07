eb61cc25e5a4fa1d5ed0529dea7ee8d3d2fa148e
Leonardo Catello <leonardo.catello23@gmail.com>
2023-12-07 19:04:04 +0100

    fix
    Co-Authored-By: Rasbon99 <58120555+Rasbon99@users.noreply.github.com>

--- Accelerator for Neural Network/acc_pipe.v
+++ Accelerator for Neural Network/acc_pipe.v
@@ -34,29 +34,33 @@ module acc_pipe(X1, X2, X3, X4, Y, ready, valid, ready_out, valid_out, clk, arst
 	parameter [11:0] n3_xmax = 12'd127;
 
 	reg [2:0] current_state;
-	parameter [2:0] S0=3'b000, S1=3'b001, S2=3'b010, S3=3'b011, S4=3'b100;
+	parameter [2:0] T0=3'b000, T1=3'b001, T2=3'b010, T3=3'b011, T4=3'b100;
 
 	wire enable1, enable2, enable3;
+
+	neuron N1 (.X1(X1), .X2(X2), .X3(X3), .X4(X4), .W1(n1_w1), .W2(n1_w2), .W3(n1_w3), .W4(n1_w4), 
+				.bias(n1_bias), .xmin(n1_xmin), .xmax(n1_xmax), .Y(Y1));
+	
+	neuron N2 (.X1(X1), .X2(X2), .X3(X3), .X4(X4), .W1(n2_w1), .W2(n2_w2), .W3(n2_w3), .W4(n2_w4), 
+				.bias(n2_bias), .xmin(n2_xmin), .xmax(n2_xmax), .Y(Y2));
+	
+	neuron N3 (.X1(S1), .X2(S2), .X3(0), .X4(0), .W1(n3_w1), .W2(n3_w2), .W3(n3_w3), .W4(n3_w4), 
+				.bias(n3_bias), .xmin(n3_xmin), .xmax(n3_xmax), .Y(Y3));
 
 	always @(posedge clk or posedge arst)
 		begin
 			if (arst)
 				begin
-					current_state <= S0;
-					ready <= 1'b1;
-					valid_out <= 1'b0;
-					enable1 <= 1'b0;
-					enable2 <= 1'b0;
-					enable3 <= 1'b0;
+					current_state <= T0;
 				end
 			else
 				begin
 					case(current_state)
-						S0: current_state <= (valid) ? S1 : S0;
-						S1: current_state <= S2;
-						S2: current_state <= S3;
-						S3: current_state <= (ready_out) ? S4 : S3;
-						S4: current_state <= S0;
+						T0: current_state <= (valid) ? T1 : T0;
+						T1: current_state <= T2;
+						T2: current_state <= T3;
+						T3: current_state <= (ready_out) ? T4 : T3;
+						T4: current_state <= T0;
 					endcase
 				end
 		end
@@ -64,53 +68,59 @@ module acc_pipe(X1, X2, X3, X4, Y, ready, valid, ready_out, valid_out, clk, arst
 	always @(current_state)
 		begin
 			case(current_state)
-				S0: 
-					enable1 <= 1'b0;
-					enable2 <= 1'b0;
-					enable3 <= 1'b0;
-					valid_out <= 1'b0;
-					ready <= 1'b1;
-				S1:
-					enable1 <= 1'b1;
-					enable2 <= 1'b1;
-					enable3 <= 1'b0;
-					valid_out <= 1'b0;
-					ready <= 1'b1;
-				S2:					
-					enable1 <= 1'b1;
-					enable2 <= 1'b1;
-					enable3 <= 1'b0;
-					valid_out <= 1'b1;
-					ready <= 1'b1;
-				S3:
-					enable1 <= 1'b0;
-					enable2 <= 1'b0;
-					enable3 <= 1'b1;
-					valid_out <= 1'b1;
-					ready <= 1'b0;
-				S4:
-					enable1 <= 1'b0;
-					enable2 <= 1'b0;
-					enable3 <= 1'b0;
-					valid_out <= 1'b1;
-					ready <= 1'b1;
+				T0: 
+					begin
+						enable1 <= 1'b0;
+						enable2 <= 1'b0;
+						enable3 <= 1'b0;
+						valid_out <= 1'b0;
+						ready <= 1'b1;
+					end
+				T1:
+					begin
+						enable1 <= 1'b1;
+						enable2 <= 1'b1;
+						enable3 <= 1'b0;
+						valid_out <= 1'b0;
+						ready <= 1'b1;
+					end
+				T2:	
+					begin				
+						enable1 <= 1'b1;
+						enable2 <= 1'b1;
+						enable3 <= 1'b0;
+						valid_out <= 1'b1;
+						ready <= 1'b1;
+					end
+				T3:
+					begin
+						enable1 <= 1'b0;
+						enable2 <= 1'b0;
+						enable3 <= 1'b1;
+						valid_out <= 1'b1;
+						ready <= 1'b0;
+					end
+				T4:
+					begin
+						enable1 <= 1'b0;
+						enable2 <= 1'b0;
+						enable3 <= 1'b0;
+						valid_out <= 1'b1;
+						ready <= 1'b1;
+					end
 				default:
-            		enable1 <= 1'bx;
-            		enable2 <= 1'bx;
-            		enable3 <= 1'bx;
-            		valid_out <= 1'bx;
-            		ready <= 1'bx;
+					begin
+						enable1 <= 1'bx;
+						enable2 <= 1'bx;
+						enable3 <= 1'bx;
+						valid_out <= 1'bx;
+						ready <= 1'bx;
+					end
 			endcase
 		end
 
 	always @(posedge clk)
 		begin
-
-			N1 neuron (.X1(X1), .X2(X2), .X3(X3), .X4(X4), .W1(n1_w1), .W2(n1_w2), .W3(n1_w3), .W4(n1_w4), 
-						.bias(n1_bias), .xmin(n1_xmin), .xmax(n1_xmax), .Y(Y1));
-
-			N2 neuron (.X1(X1), .X2(X2), .X3(X3), .X4(X4), .W1(n2_w1), .W2(n2_w2), .W3(n2_w3), .W4(n2_w4), 
-						.bias(n2_bias), .xmin(n2_xmin), .xmax(n2_xmax), .Y(Y2));
 	
 			if(enable1) 
 				begin
@@ -120,9 +130,6 @@ module acc_pipe(X1, X2, X3, X4, Y, ready, valid, ready_out, valid_out, clk, arst
 						begin
 
 							assign S2 = Y2;
-
-							N3 neuron (.X1(S1), .X2(S2), .X3(0), .X4(0), .W1(n3_w1), .W2(n3_w2), .W3(n3_w3), .W4(n3_w4), 
-										.bias(n3_bias), .xmin(n3_xmin), .xmax(n3_xmax), .Y(Y3));
 
 							if(enable3)
 								begin

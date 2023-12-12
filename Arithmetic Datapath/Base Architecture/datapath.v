21fa68e63ae8013d71437d2841db2be1bb30522d
Rasbon99 <lollo.manco@gmail.com>
2023-12-12 10:25:48 +0100

    Update datapath.v

--- Arithmetic Datapath/Base Architecture/datapath.v
+++ Arithmetic Datapath/Base Architecture/datapath.v
@@ -13,7 +13,7 @@ module datapath (A, B, opcode, Y, co);
   // MUX 2: Scelta tra l'uscita negata del MUX1 e l'uscita del MUX1
   assign output_mux2 = (opcode[1]) ? ~output_mux1 : output_mux1;
 
-  // ADDER: Se generato il diciasettesimo bit va nella concatenazione in carry-out 
+  // ADDER: Se generato il diciassettesimo bit va nella concatenazione in carry-out 
   assign {co,Y} = (A + output_mux2 + opcode[0]);
 
 endmodule
